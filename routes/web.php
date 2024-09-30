fd7a7e29a14069daaca368b0acd577ebbb2d7cc8
Dewa Yoga <88034694+DewaYoGamer@users.noreply.github.com>
2024-09-30 13:41:12 +0800

    add some

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/test2', function () {
+    return view('test2');
+});
