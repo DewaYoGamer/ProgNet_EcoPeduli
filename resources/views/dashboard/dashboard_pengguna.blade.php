62421f250e63e27824c897bb772a555f76629218
Nyoman Wiprayanka <nyomantayo@gmail.com>
2024-10-02 03:02:12 +0800

    Setengah Jadi Dashboard Pengguna

--- resources/views/dashboard/dashboard_pengguna.blade.php
+++ resources/views/dashboard/dashboard_pengguna.blade.php
@@ -11,7 +11,7 @@
 <body>
     <div class="flex">
         {{-- SIDEBAR --}}
-        <div class="bg-[#50623A] h-screen p-5 pt-5 w-[300px]">
+        <div class="bg-[#50623A] h-screen p-5 pt-5 z-[999] w-[300px]">
             {{-- Logo --}}
             <div class="flex w-auto h-32 p-3 items-center flex-col">
                 <img src="{{asset('images/logo.png')}}" class="h-16" alt="">
@@ -50,6 +50,34 @@
             {{-- End Opsi Bawah --}}
         </div>
         {{-- END SIDEBAR --}}
+
+        {{-- Konten Atas --}}
+        <nav class="bg-[#789461] fixed w-full h-20 z-20 top-0 start-0">
+            
+        </nav>
+
+        <div class="flex flex-col space-y-5 p-5 w-[640px] h-[320px] mt-[125px] ml-[50px] bg-[#f1cc1b] rounded-[20px] justify-center">
+            <img src="{{asset('images/gambar_fotoprofil_rz.png')}}" class="max-w-[160px]" alt="">
+            <div class="text-[#fafafa] font-bold text-3xl">
+                <p>@username</p>
+            </div>
+            <div class="text-[#fafafa] font-bold text-4xl">
+                <p class="-mt-2">Nama Lengkap</p>
+            </div>
+        </div>
+
+        <div class="flex flex-col space-y-5 p-5 w-[640px] h-[320px] mt-[125px] ml-[200px] bg-[#f1cc1b] rounded-[20px] justify-center items-center">
+            <div class="text-[#fafafa] font-bold text-3xl">
+                <p>Rabu, 02 Oktober 2024</p>
+            </div>
+            <div class="text-[#fafafa] font-bold text-4xl">
+                <p class="-mt-2">Kumpulkan Sampah: </p>
+            </div>
+            <div class="text-[#fafafa] font-bold text-4xl">
+                <p class="-mt-2">Daur Ulang </p>
+            </div>
+        </div>
+        {{-- End Konten Atas --}}
     </div>
 </body>
 </html>
