919e39311153732e6aa299713d163bd95d948b60
Dewa Yoga <88034694+DewaYoGamer@users.noreply.github.com>
2024-10-02 17:11:17 +0800

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Document</title>
+    <title>INI TEST</title>
 </head>
-<body>
-    <div class="font-ComicLemon">
-        This text uses the custom font.
+<body class="flex flex-col">
+    <div class="w-full h-[33vh] bg-black">
+    </div>
+    <div class="w-full h-[33vh] bg-red-600">
+    </div>
+    <div class="w-full h-[34vh] bg-yellow-500">
     </div>
 </body>
 </html>
