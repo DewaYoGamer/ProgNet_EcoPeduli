3e188dbc9a2fc8c294ccf818ae434f31aec28447
Dewa Yoga <88034694+DewaYoGamer@users.noreply.github.com>
2024-10-07 02:14:43 +0800

    test about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,10 @@
 <x-layout>
     <x-slot name="title">Tentang | Eco Peduli</x-slot>
-    <h1 class="text-3xl font-bold">Ini Tentang</h1>
+    <div class="p-32">
+        <div class="">
+            <div class="w-[50rem] text-center lg:text-left self-stretch text-black text-3xl lg:text-5xl font-bold">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, qui.
+            </div>
+        </div>
+    </div>
 </x-layout>
