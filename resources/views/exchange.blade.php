52c2f0c022eed6456be101f1bbe71dd90973e8f0
Nyoman Wiprayanka <nyomantayo@gmail.com>
2024-10-08 14:38:16 +0800

    Add Top Text

--- resources/views/exchange.blade.php
+++ resources/views/exchange.blade.php
@@ -1,4 +1,13 @@
 <x-layout>
     <x-slot name="title">Tukar | Eco Peduli</x-slot>
-    <h1 class="text-3xl font-bold">Ini Beranda</h1>
+    <div class="flex flex-col">
+        <div class="flex flex-col justify-center items-center pt-6">
+            <div class="text-center self-stretch text-black text-3xl lg:text-5xl font-bold xl:leading-tight">
+                Ayo Tukarkan <span class="text-primary">Poinmu!</span>
+            </div>
+            <div class="font mt-1 text-center">
+                Tukarkan <span class="text-primary font-bold">poin</span> yang anda miliki menjadi berbagai <span class="text-primary font-bold">sembako</span> yang bermanfaat
+            </div>
+        </div>
+    </div>
 </x-layout>
