bf15772696df1b9729a50cc396e85c8e7bd16810
aryaarinatha <13.iketutaryaarinathawardana@gmail.com>
2024-10-23 03:29:07 +0800

    new update edukasi

--- resources/views/educate_pemilahan.blade.php
+++ resources/views/educate_pemilahan.blade.php
@@ -12,15 +12,22 @@
                 <div class="bg-[#FAFAFA] h-full w-full rounded-xl flex flex-col p-10 items-left justify-between border-slate-200 border-2">
                     <div class="flex flex-col">
                         <img src="{{ asset('images/pemilahan1.jpg') }}" class="w-full h-[30rem] rounded-[12px] object-cover object-top">
-                        <p class="font-normal text-justify">
-                            <span class="mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik1-btn">1. Apa Pentingnya Pemilahan Sampah dalam Kehidupan Sehari-hari?</span>
-                            <br>
-                            <span class="mt-4 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik2-btn">2. Bagaimana Cara Melakukan Pemilahan Sampah yang Efektif?</span>
-                            <br>
-                            <span class="mt-4 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik3-btn">3. Apa Tantangan yang Dihadapi dalam Pemilahan Sampah di Masyarakat?</span>
-                            <br>
-                            <span class="mt-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik4-btn">4. Apa Manfaat Daur Ulang dalam Konteks Pemilahan Sampah?</span>
-                        </p>
+                            <div class="flex flex-row mt-8 items-center">
+                                <img src="{{ asset('images/Asset 1.png') }}" class="w-[4rem] h-[6rem] mr-8">
+                                <p class="text-justify mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik1-btn">Apa Pentingnya Pemilahan Sampah dalam Kehidupan Sehari-hari?</p>
+                            </div>
+                            <div class="flex flex-row mt-8 items-center">
+                                <img src="{{ asset('images/Asset 2.png') }}" class="w-[4rem] h-[5rem] mr-8">
+                                <p class="text-justify mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik2-btn">Bagaimana Cara Melakukan Pemilahan Sampah yang Efektif?</p>
+                            </div>
+                            <div class="flex flex-row mt-8 items-center">
+                                <img src="{{ asset('images/Asset 3.png') }}" class="w-[4rem] h-[4rem] mr-8">
+                                <p class="text-justify mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik3-btn">Apa Tantangan yang Dihadapi dalam Pemilahan Sampah di Masyarakat?</p>
+                            </div>
+                            <div class="flex flex-row mt-8 items-center">
+                                <img src="{{ asset('images/Asset 4.png') }}" class="w-[4rem] h-[4rem] mr-8">
+                                <p class="text-justify mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik4-btn">Apa Manfaat Daur Ulang dalam Konteks Pemilahan Sampah?</p>
+                            </div>     
                     </div>
                     <a href="/educate" class="bg-lime-500 w-full text-center p-3 rounded-[8px] text-white font-bold text-xl mt-8">
                         KEMBALI
@@ -32,10 +39,15 @@
         <!-- Modal overlay for Topik 1 -->
         <div id="topik1-modal" class="fixed inset-0 hidden z-50 items-center justify-center bg-black bg-opacity-50">
             <div class="bg-white rounded-lg w-[90rem] p-8">
-                <h2 class="text-4xl font-bold mb-4">1. Apa Pentingnya Pemilahan Sampah dalam Kehidupan Sehari-hari?</h2>
-                <p class="text-2xl">
-                    Pemilahan sampah penting untuk mengurangi jumlah limbah yang masuk ke tempat pembuangan akhir (TPA), sehingga dapat memperpanjang umur TPA dan mengurangi dampak negatif terhadap lingkungan. Dengan memisahkan sampah organik dan anorganik, kita dapat mengoptimalkan proses daur ulang dan pengelolaan limbah, serta mengurangi polusi dan emisi gas rumah kaca.
-                </p>
+                <div class="flex flex-row items-center">
+                    <img src="{{ asset('images/Asset 1.png') }}" class="w-[10rem] h-[11rem] mr-8">
+                    <div class="flex flex-col">
+                            <h2 class="text-4xl font-bold mb-4">Apa Pentingnya Pemilahan Sampah dalam Kehidupan Sehari-hari?</h2>
+                            <p class="text-2xl">
+                                Pemilahan sampah penting untuk mengurangi jumlah limbah yang masuk ke tempat pembuangan akhir (TPA), sehingga dapat memperpanjang umur TPA dan mengurangi dampak negatif terhadap lingkungan. Dengan memisahkan sampah organik dan anorganik, kita dapat mengoptimalkan proses daur ulang dan pengelolaan limbah, serta mengurangi polusi dan emisi gas rumah kaca.
+                            </p>
+                    </div>
+                </div>
                 <button id="close-modal1" class="bg-lime-500 font-bold text-white px-4 py-2 rounded-lg mt-6">Tutup</button>
             </div>
         </div>
@@ -43,21 +55,31 @@
         <!-- Modal overlay for Topik 2 -->
         <div id="topik2-modal" class="fixed inset-0 hidden z-50 items-center justify-center bg-black bg-opacity-50">
             <div class="bg-white rounded-lg w-[90rem] p-8">
-                <h2 class="text-4xl font-bold mb-4">2. Bagaimana Cara Melakukan Pemilahan Sampah yang Efektif?</h2>
-                <p class="text-2xl">
-                    Melakukan pemilahan sampah yang efektif di rumah dapat dimulai dengan menyediakan beberapa wadah terpisah untuk sampah organik, anorganik, dan daur ulang. Pastikan untuk memberikan informasi yang jelas kepada anggota keluarga mengenai jenis sampah yang harus dibuang ke masing-masing wadah.
-                </p>
+                <div class="flex flex-row items-center">
+                    <img src="{{ asset('images/Asset 2.png') }}" class="w-[10rem] h-[11rem] mr-8">
+                    <div class="flex flex-col">
+                            <h2 class="text-4xl font-bold mb-4">Bagaimana Cara Melakukan Pemilahan Sampah yang Efektif?</h2>
+                            <p class="text-2xl">
+                                Melakukan pemilahan sampah yang efektif di rumah dapat dimulai dengan menyediakan beberapa wadah terpisah untuk sampah organik, anorganik, dan daur ulang. Pastikan untuk memberikan informasi yang jelas kepada anggota keluarga mengenai jenis sampah yang harus dibuang ke masing-masing wadah.
+                            </p>
+                    </div>
+                </div>
                 <button id="close-modal2" class="bg-lime-500 font-bold text-white px-4 py-2 rounded-lg mt-6">Tutup</button>
             </div>
         </div>
 
         <!-- Modal overlay for Topik 3 -->
         <div id="topik3-modal" class="fixed inset-0 hidden z-50 items-center justify-center bg-black bg-opacity-50">
-            <div class="bg-white rounded-lg w-[95rem] p-8">
-                <h2 class="text-4xl font-bold mb-4">3. Apa Tantangan yang Dihadapi dalam Pemilahan Sampah di Masyarakat?</h2>
-                <p class="text-2xl">
-                    Tantangan dalam pemilahan sampah di masyarakat meliputi kurangnya kesadaran dan edukasi mengenai pentingnya pemilahan, fasilitas pengelolaan sampah yang tidak memadai, dan kebiasaan masyarakat yang sulit diubah.
-                </p>
+            <div class="bg-white rounded-lg w-[90rem] p-8">
+                <div class="flex flex-row items-center">
+                    <img src="{{ asset('images/Asset 3.png') }}" class="w-[10rem] h-[10rem] mr-8">
+                    <div class="flex flex-col">
+                            <h2 class="text-4xl font-bold mb-4">Apa Tantangan yang Dihadapi dalam Pemilahan Sampah di Masyarakat?</h2>
+                            <p class="text-2xl">
+                                Tantangan dalam pemilahan sampah di masyarakat meliputi kurangnya kesadaran dan edukasi mengenai pentingnya pemilahan, fasilitas pengelolaan sampah yang tidak memadai, dan kebiasaan masyarakat yang sulit diubah.
+                            </p>
+                    </div>
+                </div>
                 <button id="close-modal3" class="bg-lime-500 font-bold text-white px-4 py-2 rounded-lg mt-6">Tutup</button>
             </div>
         </div>
@@ -65,15 +87,18 @@
         <!-- Modal overlay for Topik 4 -->
         <div id="topik4-modal" class="fixed inset-0 hidden z-50 items-center justify-center bg-black bg-opacity-50">
             <div class="bg-white rounded-lg w-[90rem] p-8">
-                <h2 class="text-4xl font-bold mb-4">4. Apa Manfaat Daur Ulang dalam Konteks Pemilahan Sampah?</h2>
-                <p class="text-2xl">
-                    Daur ulang memiliki banyak manfaat, seperti mengurangi jumlah sampah yang masuk ke TPA, menghemat sumber daya alam, dan mengurangi pencemaran lingkungan. Daur ulang juga dapat menciptakan lapangan kerja baru dalam industri pengelolaan limbah.
-                </p>
+                <div class="flex flex-row items-center">
+                    <img src="{{ asset('images/Asset 4.png') }}" class="w-[10rem] h-[10rem] mr-8">
+                    <div class="flex flex-col">
+                            <h2 class="text-4xl font-bold mb-4">Apa Manfaat Daur Ulang dalam Konteks Pemilahan Sampah?</h2>
+                            <p class="text-2xl">
+                                Daur ulang memiliki banyak manfaat, seperti mengurangi jumlah sampah yang masuk ke TPA, menghemat sumber daya alam, dan mengurangi pencemaran lingkungan. Daur ulang juga dapat menciptakan lapangan kerja baru dalam industri pengelolaan limbah.
+                            </p>
+                    </div>
+                </div>
                 <button id="close-modal4" class="bg-lime-500 font-bold text-white px-4 py-2 rounded-lg mt-6">Tutup</button>
             </div>
         </div>
-
-
         <x-footer></x-footer>
     </div>
 
--- resources/views/educate_pengelolaan.blade.php
+++ resources/views/educate_pengelolaan.blade.php
@@ -12,15 +12,22 @@
                 <div class="bg-[#FAFAFA] h-full w-full rounded-xl flex flex-col p-10 items-left justify-between border-slate-200 border-2">
                     <div class="flex flex-col">
                         <img src="{{ asset('images/pengelolaan1.jpg') }}" class="w-full h-[30rem] rounded object-cover object-top">
-                            <p class="font-normal text-justify">
-                                <span class="mt-10 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik1-btn">1. Bagaimana Cara Pengelolaan Sampah yang Baik dan Benar di Rumah Tangga?</span>
-                                <br>
-                                <span class="mt-4 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik2-btn">2. Apa dampak dari pengelolaan sampah yang buruk terhadap lingkungan?</span>
-                                <br>
-                                <span class="mt-4 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik3-btn">3. Apa Tantangan dalam Pengelolaan Sampah dan Bagaimana Mengatasinya?</span>
-                                <br>
-                                <span class="mt-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik4-btn">4. Apa Peran Bank Sampah dalam Pengelolaan Sampah dan Sistem Kerjanya?</span>
-                            </p>
+                        <div class="flex flex-row mt-8 items-center">
+                            <img src="{{ asset('images/Asset 6.png') }}" class="w-[4rem] h-[6rem] mr-8">
+                            <p class="text-justify mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik1-btn">Bagaimana Cara Pengelolaan Sampah yang Baik dan Benar di Rumah Tangga?</p>
+                        </div>
+                        <div class="flex flex-row mt-8 items-center">
+                            <img src="{{ asset('images/Asset 5.png') }}" class="w-[4rem] h-[2rem] mr-8">
+                            <p class="text-justify mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik2-btn">Apa dampak dari pengelolaan sampah yang buruk terhadap lingkungan?</p>
+                        </div>
+                        <div class="flex flex-row mt-8 items-center">
+                            <img src="{{ asset('images/Asset 3.png') }}" class="w-[4rem] h-[4rem] mr-8">
+                            <p class="text-justify mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik3-btn">Apa Tantangan dalam Pengelolaan Sampah dan Bagaimana Mengatasinya?</p>
+                        </div>
+                        <div class="flex flex-row mt-8 items-center">
+                            <img src="{{ asset('images/Asset 7.png') }}" class="w-[4rem] h-[4rem] mr-8">
+                            <p class="text-justify mt-5 mb-4 inline-block cursor-pointer text-primary text-2xl font-semibold hover:underline" id="topik4-btn">Apa Peran Bank Sampah dalam Pengelolaan Sampah dan Sistem Kerjanya?</p>
+                        </div>     
                     </div>
                     <a href="/educate" class="bg-lime-500 w-full text-center p-3 rounded-[8px] text-white font-bold text-xl my-10">
                         KEMBALI
@@ -32,10 +39,15 @@
         <!-- Modal overlay for Topik 1 -->
         <div id="topik1-modal" class="fixed inset-0 hidden z-50 items-center justify-center bg-black bg-opacity-50">
             <div class="bg-white rounded-lg w-[90rem] p-8">
-                <h2 class="text-4xl font-bold mb-4">1. Bagaimana Cara Pengelolaan Sampah yang Baik dan Benar di Rumah Tangga?</h2>
-                <p class="text-2xl">
-                    Cara Pengelolaan Sampah yang Baik dan Benar di Rumah Tangga adalah dengan memisahkan sampah organik, anorganik, dan berbahaya, serta menerapkan prinsip 3R (Reduce, Reuse, Recycle). Sampah organik dapat dijadikan kompos, sedangkan sampah anorganik seperti plastik, kertas, dan logam dapat didaur ulang. Mengurangi penggunaan barang sekali pakai dan membuang sampah berbahaya (B3) di tempat yang benar juga penting untuk menjaga kebersihan dan kesehatan lingkungan.
-                </p>
+                <div class="flex flex-row items-center">
+                    <img src="{{ asset('images/Asset 6.png') }}" class="w-[10rem] h-[11rem] mr-8">
+                    <div class="flex flex-col">
+                            <h2 class="text-4xl font-bold mb-4">Bagaimana Cara Pengelolaan Sampah yang Baik dan Benar di Rumah Tangga?</h2>
+                            <p class="text-2xl">
+                                Cara Pengelolaan Sampah yang Baik dan Benar di Rumah Tangga adalah dengan memisahkan sampah organik, anorganik, dan berbahaya, serta menerapkan prinsip 3R (Reduce, Reuse, Recycle). Sampah organik dapat dijadikan kompos, sedangkan sampah anorganik seperti plastik, kertas, dan logam dapat didaur ulang. Mengurangi penggunaan barang sekali pakai dan membuang sampah berbahaya (B3) di tempat yang benar juga penting untuk menjaga kebersihan dan kesehatan lingkungan.
+                            </p>
+                    </div>
+                </div>
                 <button id="close-modal1" class="bg-lime-500 font-bold text-white px-4 py-2 rounded-lg mt-6">Tutup</button>
             </div>
         </div>
@@ -43,21 +55,31 @@
         <!-- Modal overlay for Topik 2 -->
         <div id="topik2-modal" class="fixed inset-0 hidden z-50 items-center justify-center bg-black bg-opacity-50">
             <div class="bg-white rounded-lg w-[90rem] p-8">
-                <h2 class="text-4xl font-bold mb-4">2. Apa dampak dari pengelolaan sampah yang buruk terhadap lingkungan?</h2>
-                <p class="text-2xl">
-                    Dampak dari Pengelolaan Sampah yang Buruk terhadap Lingkungan meliputi pencemaran tanah, air, dan udara akibat sampah yang tidak terolah dengan baik, terutama plastik dan limbah berbahaya. Sampah yang tidak terkelola dapat menyebabkan banjir, merusak ekosistem, dan mengancam kesehatan manusia dengan memicu penyakit yang disebabkan oleh limbah beracun atau polusi udara dari pembakaran sampah.
-                </p>
+                <div class="flex flex-row items-center">
+                    <img src="{{ asset('images/Asset 5.png') }}" class="w-[10rem] h-[5rem] mr-8">
+                    <div class="flex flex-col">
+                            <h2 class="text-4xl font-bold mb-4">Apa dampak dari pengelolaan sampah yang buruk terhadap lingkungan?</h2>
+                            <p class="text-2xl">
+                                Dampak dari Pengelolaan Sampah yang Buruk terhadap Lingkungan meliputi pencemaran tanah, air, dan udara akibat sampah yang tidak terolah dengan baik, terutama plastik dan limbah berbahaya. Sampah yang tidak terkelola dapat menyebabkan banjir, merusak ekosistem, dan mengancam kesehatan manusia dengan memicu penyakit yang disebabkan oleh limbah beracun atau polusi udara dari pembakaran sampah.
+                            </p>
+                    </div>
+                </div>
                 <button id="close-modal2" class="bg-lime-500 font-bold text-white px-4 py-2 rounded-lg mt-6">Tutup</button>
             </div>
         </div>
 
         <!-- Modal overlay for Topik 3 -->
         <div id="topik3-modal" class="fixed inset-0 hidden z-50 items-center justify-center bg-black bg-opacity-50">
-            <div class="bg-white rounded-lg w-[95rem] p-8">
-                <h2 class="text-4xl font-bold mb-4">3. Apa Tantangan Utama dalam Pengelolaan Sampah di Perkotaan dan Bagaimana Cara Mengatasinya?</h2>
-                <p class="text-2xl">
-                    Tantangan Utama dalam Pengelolaan Sampah di Perkotaan adalah meningkatnya volume sampah, kurangnya kesadaran masyarakat dalam memilah sampah, terbatasnya infrastruktur pengolahan, serta pengelolaan sampah plastik yang sulit didaur ulang. Untuk mengatasi hal ini, diperlukan edukasi yang lebih luas, peningkatan fasilitas daur ulang, kebijakan pengurangan plastik sekali pakai, serta pengembangan teknologi pengolahan sampah yang lebih efisien.
-                </p>
+            <div class="bg-white rounded-lg w-[90rem] p-8">
+                <div class="flex flex-row items-center">
+                    <img src="{{ asset('images/Asset 3.png') }}" class="w-[10rem] h-[10rem] mr-8">
+                    <div class="flex flex-col">
+                            <h2 class="text-4xl font-bold mb-4">Apa Tantangan Utama dalam Pengelolaan Sampah di Perkotaan dan Bagaimana Cara Mengatasinya?</h2>
+                            <p class="text-2xl">
+                                Tantangan Utama dalam Pengelolaan Sampah di Perkotaan adalah meningkatnya volume sampah, kurangnya kesadaran masyarakat dalam memilah sampah, terbatasnya infrastruktur pengolahan, serta pengelolaan sampah plastik yang sulit didaur ulang. Untuk mengatasi hal ini, diperlukan edukasi yang lebih luas, peningkatan fasilitas daur ulang, kebijakan pengurangan plastik sekali pakai, serta pengembangan teknologi pengolahan sampah yang lebih efisien.
+                            </p>
+                    </div>
+                </div>
                 <button id="close-modal3" class="bg-lime-500 font-bold text-white px-4 py-2 rounded-lg mt-6">Tutup</button>
             </div>
         </div>
@@ -65,15 +87,18 @@
         <!-- Modal overlay for Topik 4 -->
         <div id="topik4-modal" class="fixed inset-0 hidden z-50 items-center justify-center bg-black bg-opacity-50">
             <div class="bg-white rounded-lg w-[90rem] p-8">
-                <h2 class="text-4xl font-bold mb-4">4. Apa Peran Bank Sampah dalam Pengelolaan Sampah dan Bagaimana Sistem Kerjanya?</h2>
-                <p class="text-2xl">
-                    Peran Bank Sampah dalam Pengelolaan Sampah adalah membantu masyarakat dalam memilah dan menukar sampah anorganik yang dapat didaur ulang dengan uang atau barang. Bank sampah mendorong kesadaran lingkungan dengan mengurangi sampah yang masuk ke TPA, meningkatkan ekonomi lokal, dan mendukung program daur ulang dengan mendistribusikan sampah ke industri pengolahan. Sistem kerjanya melibatkan pengumpulan, pemilahan, dan penukaran sampah dari masyarakat.
-                </p>
+                <div class="flex flex-row items-center">
+                    <img src="{{ asset('images/Asset 7.png') }}" class="w-[10rem] h-[10rem] mr-8">
+                    <div class="flex flex-col">
+                            <h2 class="text-4xl font-bold mb-4">Apa Peran Bank Sampah dalam Pengelolaan Sampah dan Bagaimana Sistem Kerjanya?</h2>
+                            <p class="text-2xl">
+                                Peran Bank Sampah dalam Pengelolaan Sampah adalah membantu masyarakat dalam memilah dan menukar sampah anorganik yang dapat didaur ulang dengan uang atau barang. Bank sampah mendorong kesadaran lingkungan dengan mengurangi sampah yang masuk ke TPA, meningkatkan ekonomi lokal, dan mendukung program daur ulang dengan mendistribusikan sampah ke industri pengolahan. Sistem kerjanya melibatkan pengumpulan, pemilahan, dan penukaran sampah dari masyarakat.
+                            </p>
+                    </div>
+                </div>
                 <button id="close-modal4" class="bg-lime-500 font-bold text-white px-4 py-2 rounded-lg mt-6">Tutup</button>
             </div>
         </div>
-
-
         <x-footer></x-footer>
     </div>
 
