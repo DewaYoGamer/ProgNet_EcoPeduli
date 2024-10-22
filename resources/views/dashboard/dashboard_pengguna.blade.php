f1a17b572898c4b50f0bd6a6c4e784c456614a2c
Nyoman Wiprayanka <nyomantayo@gmail.com>
2024-10-23 01:38:59 +0800

    template dashboard

--- resources/views/dashboard/dashboard_pengguna.blade.php
+++ resources/views/dashboard/dashboard_pengguna.blade.php
@@ -29,13 +29,28 @@
             transform: rotate(180deg);
         }
 
-        .logo-text {
-            transition: opacity 0.3s ease-in-out;
+        /* Transisi untuk teks dan elemen yang menghilang */
+        .logo-text, 
+        .text-navigasi-1, 
+        .text-navigasi-2, 
+        .text-dashboard, 
+        .text-penukaran, 
+        .text-jadwal, 
+        .text-profil, 
+        .text-beranda, 
+        .text-logout {
+            transition: opacity 0.4s ease, visibility 0.4s ease;
         }
 
-        .logo-text.disappear {
-            transition: opacity 0.3s ease-in-out;
+        .disappear {
             opacity: 0;
+            visibility: hidden;
+        }
+
+        /* Ukuran font ikon yang disesuaikan */
+        .adjusting {
+            font-size: 42px;
+            transition: font-size 0.5s ease;
         }
     </style>
 </head>
@@ -58,45 +73,59 @@
 
             {{-- Opsi Atas --}}
             <ul class="pt-2">
-                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
-                    <span class="material-symbols-outlined">
+                <div class="text-navigasi-1 text-samping pt-4 text-slate-300 text-opacity-35 text-xl font-bold -mb-2">
+                    Navigasi Utama
+                </div>
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl">
+                    <span class="material-symbols-outlined block float-left dashboard">
                         dashboard
                     </span>
-                    <a href="#">Dashboard</a>
+                    <a href="#" class="text-dashboard">Dashboard</a>
                 </li>
-                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
-                    <span class="material-symbols-outlined">
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl">
+                    <span class="material-symbols-outlined penukaran">
                         currency_exchange
                     </span>
-                    <a href="#">Penukaran</a>
+                    <a href="#" class="text-penukaran">Penukaran</a>
                 </li>
-                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
-                    <span class="material-symbols-outlined">
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl">
+                    <span class="material-symbols-outlined jadwal">
                         today
                     </span>
-                    <a href="#">Jadwal</a>
+                    <a href="#" class="text-jadwal">Jadwal</a>
                 </li>
             </ul>
             {{-- End Opsi Atas --}}
 
             {{-- Opsi Bawah --}}
             <ul class="pt-2">
-                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
-                    <span class="material-symbols-outlined">
+                <div class="text-navigasi-2 pt-4 text-slate-300 text-opacity-35 text-xl font-bold -mb-2 text-samping">
+                    Pengaturan Akun
+                </div>
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl">
+                    <span class="material-symbols-outlined profil">
                         person
                     </span>
-                    <a href="#">Profil Pengguna</a>
+                    <a href="#" class="text-profil">Profil Pengguna</a>
                 </li>
-                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
-                    <a href="#">Keluar</a>
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl">
+                    <span class="material-symbols-outlined beranda">
+                        home
+                    </span>
+                    <a href="/" class="text-beranda">Beranda</a>
+                </li>
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl">
+                    <span class="material-symbols-outlined logout">
+                        logout
+                    </span>
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <button type="submit" class="text-logout">
+                            Logout
+                        </button>
+                    </form>
                 </li>
 
-                <form action="/logout" method="POST">
-                    @csrf
-                    <button type="submit" class="bg-lime-500 text-center p-3 rounded-[8px] text-white font-bold text-2xl mt-6 -mb-6">
-                        Logout
-                    </button>
-                </form>
             </ul>
             {{-- End Opsi Bawah --}}
         </div>
@@ -114,10 +143,36 @@
         document.querySelector('.ikon-min').addEventListener('click', function() {
             var sidebar = document.querySelector('.sidebar');
             var ikon = document.querySelector('.ikon-min');
-            var logo = document.querySelector('.logo-text');
+            var elementsToToggle = [
+                document.querySelector('.logo-text'),
+                document.querySelector('.text-navigasi-1'),
+                document.querySelector('.text-navigasi-2'),
+                document.querySelector('.text-dashboard'),
+                document.querySelector('.text-penukaran'),
+                document.querySelector('.text-jadwal'),
+                document.querySelector('.text-profil'),
+                document.querySelector('.text-beranda'),
+                document.querySelector('.text-logout')
+            ];
+            var iconsToResize = [
+                document.querySelector('.dashboard'),
+                document.querySelector('.penukaran'),
+                document.querySelector('.jadwal'),
+                document.querySelector('.profil'),
+                document.querySelector('.beranda'),
+                document.querySelector('.logout')
+            ];
+
             sidebar.classList.toggle('collapsed');
             ikon.classList.toggle('rotate');
-            logo.classList.toggle('disappear');
+
+            elementsToToggle.forEach(function(element) {
+                element.classList.toggle('disappear');
+            });
+
+            iconsToResize.forEach(function(icon) {
+                icon.classList.toggle('adjusting');
+            });
         });
     </script>
 </body>
