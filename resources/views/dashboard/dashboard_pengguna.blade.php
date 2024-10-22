85b467d28ef4c5ab710334b827c55878665d8fb1
Nyoman Wiprayanka <nyomantayo@gmail.com>
2024-10-23 00:37:04 +0800

    save point

--- resources/views/dashboard/dashboard_pengguna.blade.php
+++ resources/views/dashboard/dashboard_pengguna.blade.php
@@ -6,85 +6,119 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
     <title>Dashboard Pengguna | Eco Peduli</title>
+    {{-- LINK KE GOOGLE ICON --}}
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+    {{-- CSS INFILE --}}
+    <style>
+        .sidebar {
+            width: 288px; /* Default size */
+            transition: width 0.5s;
+        }
+    
+        .sidebar.collapsed {
+            width: 132px; /* Reduced size */
+        }
+
+        .ikon-min{
+            transition: transform 0.5s ease-in-out;
+        }
+
+        .ikon-min.rotate{
+            transition: transform 0.5s ease-in-out;
+            transform: rotate(180deg);
+        }
+
+        .logo-text {
+            transition: opacity 0.3s ease-in-out;
+        }
+
+        .logo-text.disappear {
+            transition: opacity 0.3s ease-in-out;
+            opacity: 0;
+        }
+    </style>
 </head>
 <body>
     <div class="flex">
         {{-- SIDEBAR --}}
-        <div class="bg-[#50623A] h-screen p-5 pt-5 z-[999] w-[240px]">
+        <div class="sidebar bg-[#50623A] h-screen p-5 pt-8 relative">
+            {{-- Tombol Arrow --}}
+            <span class="ikon-min material-symbols-outlined bg-[#fafafa] rounded-full absolute -right-4 top-9 border-4 border-[#50623A] cursor-pointer" style="font-size: 38px">
+                chevron_left
+            </span>
+            {{-- End Tombol Arrow --}}
+
             {{-- Logo --}}
-            <div class="flex w-auto h-24 p-3 items-center flex-col">
+            <div class="flex w-auto h-24 items-center flex-col">
                 <img src="{{asset('images/logo.png')}}" class="h-12" alt="">
-                <p class="mt-2 font-ComicLemon text-2xl whitespace-nowrap text-white">EcoPeduli</p>
+                <p class="logo-text mt-2 font-ComicLemon text-2xl whitespace-nowrap text-white">EcoPeduli</p>
             </div>
             {{-- End Logo --}}
 
             {{-- Opsi Atas --}}
-            <div class="flex flex-col w-auto h-56 mt-10 p-5 -ml-3">
-                <ul class="flex flex-col space-y-3">
-                    <li>
-                        <a href="#" class="{{ request()->is('pengguna') ? 'text-secondary bg-lime-600 ': 'text-white hover:bg-transparent hover:text-lime-300 hover:bg-gray-200' }} block py-2 px-3 rounded bg-transparent p-0 font-bold text-xl">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#" class="{{ request()->is('') ? 'text-secondary bg-lime-600 ': 'text-white hover:bg-transparent lg:hover:text-lime-300 hover:bg-gray-200' }} block py-2 px-3 rounded bg-transparent p-0 font-bold text-xl -mt-3 ">Penukaran</a>
-                    </li>
-                    <li>
-                        <a href="#" class="{{ request()->is('') ? 'text-secondary bg-lime-600 ': 'text-white lg:hover:bg-transparent lg:hover:text-lime-300 hover:bg-gray-200' }} block py-2 px-3 rounded bg-transparent p-0 font-bold text-xl -mt-3">Jadwal</a>
-                    </li>
-                </ul>
-            </div>
+            <ul class="pt-2">
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
+                    <span class="material-symbols-outlined">
+                        dashboard
+                    </span>
+                    <a href="#">Dashboard</a>
+                </li>
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
+                    <span class="material-symbols-outlined">
+                        currency_exchange
+                    </span>
+                    <a href="#">Penukaran</a>
+                </li>
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
+                    <span class="material-symbols-outlined">
+                        today
+                    </span>
+                    <a href="#">Jadwal</a>
+                </li>
+            </ul>
             {{-- End Opsi Atas --}}
 
             {{-- Opsi Bawah --}}
-            <div class="flex flex-col w-auto h-64 p-5 -ml-3">
-                <ul class="flex flex-col space-y-3 -mt-12">
-                    <p class="text-[#fafafa] font-bold text-xl -ml-2 w-[300px]">Pengaturan Akun</p>
-                    <li>
-                        <a href="#" class="{{ request()->is('') ? 'text-secondary bg-lime-600 ': 'text-white hover:bg-transparent lg:hover:text-lime-300 hover:bg-gray-200' }} block py-2 px-3 rounded bg-transparent p-0 font-bold text-xl ">Profil Pengguna</a>
-                    </li>
-                    <li>
-                        <a href="/" class="{{ request()->is('') ? 'text-secondary bg-lime-600 ': 'text-white lg:hover:bg-transparent lg:hover:text-lime-300 hover:bg-gray-200' }} block py-2 px-3 rounded bg-transparent p-0 font-bold text-xl -mt-3">Keluar</a>
-                        @auth
-                        <form action="/logout" method="POST">
-                            @csrf
-                            <button type="submit" class="bg-lime-500 text-center p-3 rounded-[8px] text-white font-bold text-2xl mt-6 -mb-6">
-                                Logout
-                            </button>
-                        </form>
-                        @endauth
-                    </li>
-                </ul>
-            </div>
+            <ul class="pt-2">
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
+                    <span class="material-symbols-outlined">
+                        person
+                    </span>
+                    <a href="#">Profil Pengguna</a>
+                </li>
+                <li class="{{ request()->is('') ? 'text-secondary': 'text-white hover:text-lime-300 rounded-md mt-2' }} flex items-center gap-x-4 p-2 font-bold text-xl ">
+                    <a href="#">Keluar</a>
+                </li>
+
+                <form action="/logout" method="POST">
+                    @csrf
+                    <button type="submit" class="bg-lime-500 text-center p-3 rounded-[8px] text-white font-bold text-2xl mt-6 -mb-6">
+                        Logout
+                    </button>
+                </form>
+            </ul>
             {{-- End Opsi Bawah --}}
         </div>
         {{-- END SIDEBAR --}}
 
-        {{-- Konten Atas --}}
-        <nav class="bg-[#789461] fixed w-full h-20 z-20 top-0 start-0">
-            
-        </nav>
-
-        <div class="flex flex-col space-y-5 p-5 w-[480px] h-[320px] mt-[125px] ml-[50px] bg-[#f1cc1b] rounded-[20px] justify-center">
-            <img src="{{asset('images/gambar_fotoprofil_rz.png')}}" class="max-w-[160px]" alt="">
-            <div class="text-[#fafafa] font-bold text-3xl">
-                <p>{{ auth()->user()->username  }}</p>
-            </div>
-            <div class="text-[#fafafa] font-bold text-4xl">
-                <p class="-mt-2">{{ auth()->user()->username  }}</p>
-            </div>
+        {{-- KONTEN --}}
+        <div class="text-5xl">
+            Main Page
         </div>
-
-        <div class="flex flex-col space-y-5 p-5 w-[480px] h-[320px] mt-[125px] ml-[200px] bg-[#f1cc1b] rounded-[20px] justify-center items-center">
-            <div class="text-[#fafafa] font-bold text-3xl">
-                <p>Rabu, 02 Oktober 2024</p>
-            </div>
-            <div class="text-[#fafafa] font-bold text-4xl">
-                <p class="-mt-2">Kumpulkan Sampah: </p>
-            </div>
-            <div class="text-[#fafafa] font-bold text-4xl">
-                <p class="-mt-2">Daur Ulang </p>
-            </div>
-        </div>
-        {{-- End Konten Atas --}}
+        {{-- END KONTEN --}}
     </div>
+
+    {{-- KODE JS --}}
+    <script>
+        document.querySelector('.ikon-min').addEventListener('click', function() {
+            var sidebar = document.querySelector('.sidebar');
+            var ikon = document.querySelector('.ikon-min');
+            var logo = document.querySelector('.logo-text');
+            sidebar.classList.toggle('collapsed');
+            ikon.classList.toggle('rotate');
+            logo.classList.toggle('disappear');
+        });
+    </script>
 </body>
 </html>
