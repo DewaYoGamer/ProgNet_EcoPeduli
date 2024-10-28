d732e7ae3c07df888158f0d399003423d5fa88b9
aryaarinatha <13.iketutaryaarinathawardana@gmail.com>
2024-10-28 21:14:18 +0800

    editing profile and jadwal box

--- resources/views/dashboard/dashboardPengguna_index.blade.php
+++ resources/views/dashboard/dashboardPengguna_index.blade.php
@@ -4,34 +4,46 @@
     <div class="w-full h-full bg-white shadow-md overflow-hidden">
         <!-- Header -->
 
-        <div class="w-full h-[20rem] text-center bg-gray-300 p-4 text-white flex items-center justify-between">
+        <div class="w-full h-[22rem] text-center bg-gray-300 p-6 text-white flex items-center justify-center">
             <div class="flex flex-row">
-                <div class="w-[26rem] h-[18rem] mr-20 text-left bg-yellow-500 p-6 text-white">
-                    <img src="{{ asset('images/yoga.jpg') }}" class="w-20 h-20 rounded-full object-cover">
-                    <p class="font-semibold mt-10 flex flex-col space-y-4">@wiraguna
-                        <br>
-                        <span class="font-normal">Agus Arya</span>
-                    </p>
+                <div class="w-[26rem] h-[19rem] mr-16 text-left bg-yellow-500 p-6 text-white">
+                    <div class="flex justify-center">
+                        <img src="{{ asset('images/yoga.jpg') }}" alt="Profile Picture" class="w-24 h-24 rounded-full object-cover">
+                    </div>
+                    <div class="text-left mt-3">
+                        <p class="text-gray-600 text-sm font-semibold">Username</p>
+                        <div class="bg-gray-100 border border-gray-300 w-full rounded-md px-4 py-2 mt-1 inline-block">
+                                <p class="text-gray-800 font-medium">@wiraguna</p>
+                        </div>
+                        <p class="text-gray-600 text-sm font-semibold mt-4">Nama</p>
+                        <div class="bg-gray-100 border border-gray-300 w-full rounded-md px-4 py-2 mt-1 inline-block">
+                             <p class="text-gray-800 font-medium">Agus Arya Wiraguna</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="w-[45rem] h-[18rem] text-center bg-yellow-500 p-4 text-white flex items-center justify-center">
-                    <p class="font-semibold">@wiraguna</p>
-                    <p class="text-sm">Agus Arya</p>
+                <div class="w-[45rem] h-[19rem] text-left bg-yellow-500 p-6 text-white">
+                    <div class="text-left">
+                        <p class="text-white text-3xl font-semibold">Senin,</p>
+                        <p class="text-white text-4xl font-semibold">24 Oktober 2024</p>
+                        <p class="text-white text-3xl font-semibold mt-10">Kumpulkan Sampah:</p>
+                        <p class="text-white text-4xl font-medium mt-4">Daur Ulang</p>
+                    </div>
                 </div>
             </div>
         </div>
 
         <!-- Body Content -->
-        <div class="p-6 text-left">
-            <h2 class="text-2xl font-bold mb-4">INFORMASI TERPADU</h2>
+        <div class="p-6">
+            <h2 class="text-2xl text-center font-bold mb-10">INFORMASI TERPADU</h2>
 
-            <div class="mb-4">
-                <p class="font-semibold">Kode Unik Yang Dimiliki:</p>
-                <p class="text-red-500">-</p>
+            <div class="mb-10">
+                <p class="font-semibold text-2xl">Kode Unik Yang Dimiliki:</p>
+                <p class="text-red-500 text-2xl">-</p>
             </div>
 
             <div>
-                <p class="font-semibold">Jumlah Penukaran:</p>
-                <p class="text-red-500">-</p>
+                <p class="font-semibold text-2xl">Jumlah Penukaran:</p>
+                <p class="text-red-500 text-2xl">-</p>
             </div>
         </div>
     </div>
